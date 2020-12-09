244d621b599a4f0fd42bd2d302cd2ce36ea291af
ncphung1405 <73944429+ncphung1405@users.noreply.github.com>
2020-12-09 20:15:42 +0700

    Update Bài 13-2.py

--- Bài 13-2.py
+++ Bài 13-2.py
@@ -8,6 +8,6 @@ import os
 list1=[]
 list2=[]
 for (x,y,z) in os.walk('C:\\'):
-	print(x) # a= dirpath chỉ ra những thư mục con tại ổ C
-	print(y) # b= dirnames xem tên tất cả các thư mục con
-	print(z) # c= name file xem têt tất cả các file con
+	print(x) # x= chỉ ra những thư mục con tại ổ C
+	print(y) # y= xem tên tất cả các thư mục con
+	print(z) # z= xem têt tất cả các file con
